aebf149f56a7f686ba2382f9f16c47a0b71885fd
Sunny Yang <guang.yang@thoughtworks.com>
2022-03-12 16:33:51 +0800

    config site

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '81f250c5370b45688f29bfaac9bc15cb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '7d6b4bda-13aa-4c6e-8385-834801204701',
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "Sunny's Blog",
+  domain: 'lgtm.cc',
+  author: 'Sunny Yang',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: "Yet Another Tech Enthusiast's Blog",
+  socialImageTitle: 'Sunny Yang',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: null,
+  github: 'sunnyadn',
+  linkedin: 'sunnyadn',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
